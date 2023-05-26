3c9112ea3603aa0c32868d9e56fd1c522195c606
RocioLuz17 <89813475+RocioLuz17@users.noreply.github.com>
2023-05-26 18:50:39 -0300

    first commit

---
+++ js/docjavascript.js
@@ -0,0 +1,13 @@
+//Coment
+/*Comentarios
+tipo de coment*/
+/*numero = 5;
+console.log(numero);
+const PI = 3.141516;
+
+//Limpia la consola
+console.clear();
+
+console.log("ingrese la variable");
+nombre= prompt("Ingrese nombre:");
+console.log("El nombre ingresado es:", nombre);*/
